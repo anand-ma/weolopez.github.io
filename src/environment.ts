c3dcbd22d660728c7fd03024787c6956145eef15
weolopez <weolopez@gmail.com>
2017-08-08 15:50:05 -0400

    changed

---
+++ src/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyCUjquyAUW0l6LLuyxO0WAr5KOhIplaCjs",
+        authDomain: "wiki-96483.firebaseapp.com",
+        databaseURL: "https://wiki-96483.firebaseio.com",
+        projectId: "wiki-96483",
+        storageBucket: "wiki-96483.appspot.com",
+        messagingSenderId: "1039322342866"
+    },
+    googleCloudVisionAPIKey: ""
+};
---
+++ src/pages/home/home.ts
@@ -0,0 +1,83 @@
+import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { GoogleCloudVisionServiceProvider } from '../../providers/google-cloud-vision-service/google-cloud-vision-service';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  apikey: string;
+  items: FirebaseListObservable<any[]>;
+
+  constructor(
+    private camera: Camera,
+    private vision: GoogleCloudVisionServiceProvider,
+    private db: AngularFireDatabase,
+    private alert: AlertController) {
+    this.items = db.list('items');
+    this.apikey = localStorage.getItem('apikey');
+    console.dir(this.apikey);
+  }
+
+  takePhoto(keyContainer, camera, image) {
+    const home = this;
+    if (keyContainer.value.length > 5) localStorage.setItem('apikey', keyContainer.value)
+
+    const file = camera.files[0];
+
+    let fileReader = new FileReader();
+
+    fileReader.onloadend = (event) => {
+      image.src = fileReader.result;
+      console.log(fileReader.result);
+      this.vision.getLabels(home.apikey, fileReader.result).subscribe((result) => {
+        this.saveResults(image.src, result.json().responses);
+      }, err => {
+        this.showAlert(err);
+      });
+    }
+
+    fileReader.readAsDataURL(file);
+
+  }
+
+  takeCordovaPhoto() {
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 500,
+      targetWidth: 500,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.vision.getLabels('', imageData).subscribe((result) => {
+        this.saveResults(imageData, result.json().responses);
+      }, err => {
+        this.showAlert(err);
+      });
+    }, err => {
+      this.showAlert(err);
+    });
+  }
+
+  saveResults(imageData, results) {
+    this.items.push({ imageData: imageData, results: results })
+      .then(_ => { })
+      .catch(err => { this.showAlert(err) });
+  }
+
+  showAlert(message) {
+    let alert = this.alert.create({
+      title: 'Error',
+      subTitle: message,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+}
